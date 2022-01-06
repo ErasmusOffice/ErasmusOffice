ad23920133e90b0164680318571464f17839d8b1
arensonz <echelon_erkan@hotmail.com>
2022-01-07 01:26:46 +0300

    Small database changes

--- src/main/resources/database_files/erasmus_create_tables.sql
+++ src/main/resources/database_files/erasmus_create_tables.sql
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS foreign_students (
 CREATE TABLE IF NOT EXISTS login_infos (
     username varchar(64) PRIMARY KEY,
     hashed_password varchar(100) NOT NULL,
-    role varchar(50) NOT NULL CHECK (ROLE IN ('student', 'manager'))
+    role varchar(50) NOT NULL CHECK (ROLE IN ('student', 'manager', 'it_staff'))
 );
 
 ALTER TABLE universities
