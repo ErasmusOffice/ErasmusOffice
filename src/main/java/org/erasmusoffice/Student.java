6ee425e245deaa121872ea643934526ac6a95507
MetinUsta <metin.usta01@hotmail.com>
2022-01-07 20:11:00 +0300

    Create Student model

---
+++ src/main/java/org/erasmusoffice/Student.java
@@ -0,0 +1,67 @@
+package org.erasmusoffice;
+
+public class Student {
+    private int stdID;
+    private int examResult;
+    private double GPA;
+    private String department;
+    private int consultantID;
+    private String fname;
+    private String lname;
+
+    public int getStdID() {
+        return stdID;
+    }
+
+    public void setStdID(int stdID) {
+        this.stdID = stdID;
+    }
+
+    public int getExamResult() {
+        return examResult;
+    }
+
+    public void setExamResult(int examResult) {
+        this.examResult = examResult;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getConsultantID() {
+        return consultantID;
+    }
+
+    public void setConsultantID(int consultantID) {
+        this.consultantID = consultantID;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+}
